ab8b51af62d5da44cec77747e71c27fc2f92fbbb
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-30 17:42:28 -0300

    003(DadosController_View)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,35 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function showView(): View {
-        return view("admin.newPage03");
+        // Método 01.
+        /*
+        $data = [
+            "name" => "Luciano Eduardo",
+            "phone" => "123456789"
+        ];
+
+        return view("admin.newPage03", $data);
+        */
+
+        // Método 02.
+        /*
+        return view("admin.newPage03", [
+            "name" => "Luciano Eduardo",
+            "phone" => "123456789"
+        ]);
+        */
+
+        // Método 03.
+        /*
+        return view("admin.newPage03")
+            ->with("name", "Luciano Eduardo")
+            ->with("phone", "123456789");
+        */
+
+        // Método 04.
+        $name = "Luciano Eduardo";
+        $phone = "123456789";
+
+        return view("admin.newPage03", compact("name", "phone"));
     }
 }
--- resources/views/admin/newPage03.blade.php
+++ resources/views/admin/newPage03.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts/main_layout')
 
 @section('content')
-    <p class="display-1 text-center">ADMIN</p>    
+    <h1>{{ $name }}</h1>
+    <h3>{{ $phone }}</h3>
 @endsection
