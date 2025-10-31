c4e1899cb734dbc9f362aaae0aa753e786383095
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-31 18:55:18 -0300

    006(switch)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function showView(): View {
-        return view("home", ["value" => 100]);
+        return view("home", ["value" => 500]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,21 @@
 @extends('layouts/main_layout')
 
 @section('content')
-    {{-- Instrução IF simples --}}
-    @if($value == 100)
-        <h1>Primeira parte do código</h1>
-    @endif
+    {{-- switch --}}
+    @switch($value)
+        @case(100)
+            <h1>Valor 100</h1>
+        @break
 
-    {{-- Instrução IF ELSE --}}
-    @if($value != 100)
-        <h1>Primeira parte do código</h1>
-    @else
-        <h1>Segunda parte do código</h1>
-    @endif
-
-    {{-- Instrução IF ELSEIF ELSE ENDIF --}}
-    @if($value < 10)
-        <p>Primeiro</p>
+        @case(200)
+            <h1>Valor 200</h1>
+        @break
         
-        <div>
-            <div>
-                <div>
+        @case(300)
+            <h1>Valor 300</h1>
+        @break
 
-                </div>
-            </div>
-        </div>
-    @elseif($value < 20)
-        <p>segundo</p>
-    @elseif($value > 50)
-        <p>Terceiro</p>
-    @elseif($value == 10)
-        <p>Quarto</p>
-    @else
-        <p>Outro caso</p>
-    @endif
+        @default
+            <h1>Outro!</h1>
+    @endswitch
 @endsection
